b7472ef2d85092fa618c00ceb24d624e23d8bad0
Kedir Turi <turik1@NLPULLMR3ALJG5H.local>
2021-01-22 16:40:10 -0600

    fixed the selection dependency

--- Pollen_in_US_cities/server.R
+++ Pollen_in_US_cities/server.R
@@ -1,6 +1,6 @@
 
 library(shiny)
-server <- function(input, output) {
+server <- function(session, input, output) {
   
   Average_state_pollen<- cities_pollen %>%
     group_by(state) %>%
@@ -22,20 +22,27 @@ server <- function(input, output) {
       group_by(state, city) %>% 
       arrange(state, city, desc(value), by_group=TRUE) %>% 
       slice_max(order_by = value, n=10) %>% 
-      filter(state==input$state & city==input$city) %>%
+      filter(state %in% input$state & city %in% input$city) %>%
       mutate(species = fct_reorder(species, desc(value)))
   })
   
+  observe({
+    updateSelectInput(session, 
+                      "city",
+                      choices = unique(cities_pollen_spec_long_nogeo[cities_pollen_spec_long_nogeo$state %in%
+                                                   input$state, "city"]))
+  })
+  
   output$Avrg_st_pollen <- renderPlot(
-    ggplot(Average_state_pollen) + geom_col(aes(x=reorder(state, -state_pollen), y=state_pollen),fill="grey") +
-      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 12) ) +
+    ggplot(Average_state_pollen) + geom_col(aes(x=reorder(state, -state_pollen), y=state_pollen),fill="dark blue") +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 14) ) +
       labs(title="States with total pollen count",
            x ="State", y = "Pollen count")
     )
   
   output$Avrg_cty_pollen <- renderPlot(
     ggplot(reactive_pollen()) + geom_col(aes(x=reorder(city, -city_pollen), y=city_pollen),fill="steelblue") +
-      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 12) ) +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 14) ) +
               labs(title="Cities with total pollen count",
                    x ="Cities", y = "Pollen count")
             
@@ -44,7 +51,7 @@ server <- function(input, output) {
   output$citytopspecies <- renderPlot(
     ggplot(reactive_species(), aes(species, value)) + 
     geom_col(fill="green") +
-      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 12)) +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 14)) +
       labs(title="Top pollen species",
            x ="Species", y = "Pollen count")
   )
